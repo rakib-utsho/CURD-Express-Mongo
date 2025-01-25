b6c4f844669bdac12b247290d9a97d3dfe4c8b7f
rakib-utsho <rakibutsho1920@gmail.com>
2025-01-25 22:44:33 +0600

    create route

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
 .then(()=> {
@@ -33,11 +34,31 @@ app.get("/chats", async(req, res) => {
     let chats = await Chat.find();
     res.render("index.ejs", {chats});
 });
-
+// new route
 app.get("/chats/new", (req, res)=>{
     res.render("new.ejs");
-})
+});
+// create route
+app.post("/chats", (req,res) =>{
+    let {from, to, message} = req.body;
+    let newChat = new Chat({
+        from: from,
+        to: to,
+        message: message,
+        created_at: new Date()
+    });
 
+    newChat
+    .save()
+    .then((res)=>{
+    console.log("chat was saved");
+    }).catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chats");
+});
+
+// root route
 app.get("/", (req, res)=>{
     res.send("root is working");
 });
